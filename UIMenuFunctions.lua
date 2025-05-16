c6537bc469e9ba3e2a0be1aeb69f84a258ec5e7f
ulaspbit <ulaspbit@gmail.com>
2025-05-16 15:58:27 -0600

    Se actualizo la forma en que se asignan las texturas a la parte de academia

--- UIMenuFunctions.lua
+++ UIMenuFunctions.lua
@@ -1,30 +1,41 @@
 -- UIFunctions.lua
 local LibChatMessage = LibChatMessage
 local chat = LibChatMessage("|cFF0020CerconeAddon|r", "|cFF0020CA|r")  
--- Función para cerrar panel de pjInfo
-function CerconeAddon.HideMarkTextures()
+
+Siglas = {
+  ["X"] = "CerconeAddon/Assets/Scroll/Academy/AcademiaX.dds",
+  ["AG"] = "CerconeAddon/Assets/Scroll/Academy/AcademiaAG.dds",
+  ["AL"] = "CerconeAddon/Assets/Scroll/Academy/AcademiaAL.dds",
+  ["AR"] = "CerconeAddon/Assets/Scroll/Academy/AcademiaAR.dds",
+  ["DK"] = "CerconeAddon/Assets/Scroll/Academy/AcademiaDK.dds",
+  ["HE"] = "CerconeAddon/Assets/Scroll/Academy/AcademiaHE.dds",
+  ["IC"] = "CerconeAddon/Assets/Scroll/Academy/AcademiaIC.dds",
+  ["LC"] = "CerconeAddon/Assets/Scroll/Academy/AcademiaLC.dds",
+  ["LI"] = "CerconeAddon/Assets/Scroll/Academy/AcademiaLI.dds",
+  ["NB"] = "CerconeAddon/Assets/Scroll/Academy/AcademiaNB.dds",
+  ["NE"] = "CerconeAddon/Assets/Scroll/Academy/AcademiaNE.dds",
+  ["SA"] = "CerconeAddon/Assets/Scroll/Academy/AcademiaSA.dds",
+  ["SO"] = "CerconeAddon/Assets/Scroll/Academy/AcademiaSO.dds",
+  ["TE"] = "CerconeAddon/Assets/Scroll/Academy/AcademiaTE.dds",
+  ["WA"] = "CerconeAddon/Assets/Scroll/Academy/AcademiaWA.dds",
+  ["SAN"] = "CerconeAddon/Assets/Scroll/Academy/Sanguinaris.dds",
+  ["CUS"] = "CerconeAddon/Assets/Scroll/Academy/Custode.dds",
+  ["INQ"] = "CerconeAddon/Assets/Scroll/Academy/Inquisidores.dds",
+  ["FRU"] = "CerconeAddon/Assets/Scroll/Academy/Frumentari.dds",
+}
+
+function CerconeAddon.AcademyInfo()
     local textureNames = {
       "MarkClass1", "MarkClass2", "MarkClass3", "MarkClass4", "MarkClass5", "MarkClass6", "MarkClass7", "MarkClass8", "MarkClass9",
       "MarkWar1", "MarkWar2", "MarkWar3", "MarkWar4", "MarkWar5", "MarkWar6", "MarkWar7",
       "MarkProf1", "MarkProf2", "MarkProf3",
-      "MarkLCf1", "MarkLCf2", "MarkLCf3"
+      "MarkL1", "MarkL2", "MarkL3"
     }
-    local textureNames2 = {
-      "MarkClassEmpty1", "MarkClassEmpty2", "MarkClassEmpty3", "MarkClassEmpty4", "MarkClassEmpty5", "MarkClassEmpty6", "MarkClassEmpty7", "MarkClassEmpty8", "MarkClassEmpty9",
-      "MarkWarEmpty1", "MarkWarEmpty2", "MarkWarEmpty3", "MarkWarEmpty4", "MarkWarEmpty5", "MarkWarEmpty6", "MarkWarEmpty7",
-      "MarkProfEmpty1", "MarkProfEmpty2", "MarkProfEmpty3",
-      "MarkLCfEmpty1", "MarkLCfEmpty2", "MarkLCfEmpty3"
-    }
+
     for _, textureName in ipairs(textureNames) do
       local texture = WINDOW_MANAGER:GetControlByName(textureName)
       if texture then
-        texture:SetHidden(true)
-      end
-    end
-    for _, textureName in ipairs(textureNames2) do
-      local texture = WINDOW_MANAGER:GetControlByName(textureName)
-      if texture then
-        texture:SetHidden(false)
+        texture:SetTexture("CerconeAddon/Assets/Scroll/Academy/AcademiaVacio.dds")
       end
     end
   end
@@ -32,53 +43,19 @@ function CerconeAddon.HideMarkTextures()
   function CerconeAddon.ClosePjUI()
     local panel = WINDOW_MANAGER:GetControlByName("CerconePjSimpleUI")
     if panel then
-      CerconeAddon.HideMarkTextures()
+      CerconeAddon.AcademyInfo()
       panel:SetHidden(true)
     else
       chat:Print("No se encontró el control 'CerconePjSimpleUI'")
     end
   end
   
-  function CerconeAddon.MarkClassSkillsFromValue(classSkills)
+  function SetAcademyTexture(classSkills, textureName)
     for i, item in ipairs(classSkills) do
-      local mark = WINDOW_MANAGER:GetControlByName("MarkClass" .. i)
-      local emptyMark = WINDOW_MANAGER:GetControlByName("MarkClassEmpty" .. i)
+      local mark = WINDOW_MANAGER:GetControlByName(textureName .. i)
       if item ~= "" then
-        mark:SetHidden(false)
-        emptyMark:SetHidden(true)
-      end
-    end
-  end
-  
-  function CerconeAddon.MarkWarSkillsFromValue(classSkills)
-    for i, item in ipairs(classSkills) do
-      local mark = WINDOW_MANAGER:GetControlByName("MarkWar" .. i)
-      local emptyMark = WINDOW_MANAGER:GetControlByName("MarkWarEmpty" .. i)
-      if item ~= "" then
-        mark:SetHidden(false)
-        emptyMark:SetHidden(true)
-      end
-    end
-  end
-  
-  function CerconeAddon.MarkProfSkillsFromValue(classSkills)
-    for i, item in ipairs(classSkills) do
-      local mark = WINDOW_MANAGER:GetControlByName("MarkProf" .. i)
-      local emptyMark = WINDOW_MANAGER:GetControlByName("MarkProfEmpty" .. i)
-      if item ~= "" then
-        mark:SetHidden(false)
-        emptyMark:SetHidden(true)
-      end
-    end
-  end
-  
-  function CerconeAddon.MarkLCSkillsFromValue(classSkills)
-    for i, item in ipairs(classSkills) do
-      local mark = WINDOW_MANAGER:GetControlByName("MarkLCf" .. i)
-      local emptyMark = WINDOW_MANAGER:GetControlByName("MarkLCfEmpty" .. i)
-      if item ~= "" then
-        mark:SetHidden(false)
-        emptyMark:SetHidden(true)
+          local ucItem = string.upper(item)
+          mark:SetTexture(Siglas[ucItem])
       end
     end
   end
@@ -97,7 +74,7 @@ function CerconeAddon.HideMarkTextures()
   function CerconeAddon.ShowSimpleData(name)
     local numPj = CerconeAddon.SearchpjByName(name)
     local index = tonumber(numPj)
-    CerconeAddon.HideMarkTextures()
+    CerconeAddon.AcademyInfo()
     if index and index >= 1 and index <= #CerconePjData then
       local pj = CerconePjData[index]
       chat:Print("Abriendo menu simple de: " .. pj.Personaje)
@@ -128,10 +105,10 @@ function CerconeAddon.HideMarkTextures()
         PjFuerza:SetText("Fuerza : +" .. pj.HabilidadesNOCombatientes.Fuerza)
         PjClaseAc:SetText(pj.DataGeneral.Clase .. ":")
         PjProfesion:SetText(pj.DataGeneral.Profesion .. ":")
-        CerconeAddon.MarkClassSkillsFromValue(pj.HabilidadesCombatientes.LeccionesClase)
-        CerconeAddon.MarkWarSkillsFromValue(pj.HabilidadesCombatientes.ArteDeGuerra)
-        CerconeAddon.MarkProfSkillsFromValue(pj.ProfLevel)
-        CerconeAddon.MarkLCSkillsFromValue(pj.HabilidadesCombatientes.LinajeCercone)
+        SetAcademyTexture(pj.HabilidadesCombatientes.LeccionesClase, "MarkClass")
+        SetAcademyTexture(pj.HabilidadesCombatientes.ArteDeGuerra, "MarkWar")
+        SetAcademyTexture(pj.ProfLevel, "MarkProf")
+        SetAcademyTexture(pj.HabilidadesCombatientes.LinajeCercone, "MarkL")
         panel:SetHidden(false)
       else
         chat:Print("No se encontró el control 'CerconePjSimpleUI'")
