17025a1f5f2e02a035b59633409ad99cb8693cc4
ulaspbit <ulaspbit@gmail.com>
2025-06-02 16:57:40 -0600

    Se añadio el paginado al tablon de misiones

--- TablonMisionesFunctions.lua
+++ TablonMisionesFunctions.lua
@@ -11,8 +11,10 @@ Estilos = {
   ["Sanguinaris"] = "CerconeAddon/Assets/MissionBoard/PergaminoSanguinaris.dds",
 }
 
+CerconeAddon.currentPage = 0
 
 function CerconeAddon.ShowMissionBoard(page)
+  CerconeAddon.currentPage = page or 1
   local boardData = CerconeTablonMisiones
   if not page then page = 1 end
   if not boardData or #boardData == 0 then
@@ -20,13 +22,26 @@ function CerconeAddon.ShowMissionBoard(page)
     return
   end
 
-  for i, mission in pairs(boardData) do
-    if mission.Pagina == page and mission.Texto ~= "" then
-      local scroll = GetControl("Pergamino" .. mission.Slot)
-      local title = GetControl("T" .. mission.Slot)
-      local text = GetControl("Texto" .. mission.Slot)
-      local requirements = GetControl("Req" .. mission.Slot)
+  local totalPages = 0
 
+  for i, mission in pairs(boardData) do
+
+    if mission.Texto ~= "" then
+      totalPages = mission.Pagina > totalPages and mission.Pagina or totalPages
+    end
+    if mission.Pagina > page then break end
+
+    local scroll = GetControl("Pergamino" .. mission.Slot)
+    local title = GetControl("T" .. mission.Slot)
+    local text = GetControl("Texto" .. mission.Slot)
+    local requirements = GetControl("Req" .. mission.Slot)
+
+    scroll:SetHidden(true)
+    title:SetText("")
+    text:SetText("")
+    requirements:SetText("")
+
+    if mission.Pagina == page and mission.Texto ~= "" then
       scroll:SetTexture(Estilos[mission.Estilo])
       title:SetText(mission.Titulo)
       text:SetText(mission.Texto)
@@ -35,4 +50,24 @@ function CerconeAddon.ShowMissionBoard(page)
       scroll:SetHidden(false)
     end
   end
+
+  local nextButton = GetControl("FlechaDER")
+  local prevButton = GetControl("FlechaIZQ")
+
+  if totalPages > page then
+    nextButton:SetHidden(false)
+  elseif totalPages == page then
+    nextButton:SetHidden(true)
+  end
+
+  if page > 1 then
+    prevButton:SetHidden(false)
+  else
+    prevButton:SetHidden(true)
+  end
+end
+
+function CerconeAddon.ChangePage(value)
+  local page = CerconeAddon.currentPage + value
+  CerconeAddon.ShowMissionBoard(page)
 end
